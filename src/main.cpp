3d21427076059ad42c9436fcb5b9fa7510e2e25b
Colin Williams <colinw@nc.rr.com>
2017-06-11 08:18:52 -0400

    new files

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,33 @@ main(int argc, char **argv)
 {
   QApplication app(argc, argv);
 
+  std::string filename;
+  bool        binary = false;
+
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+
+    if (arg == "-bin") {
+      ++i;
+
+      if (i < argc) {
+        filename = argv[i];
+        binary   = true;
+      }
+    }
+    else {
+      filename = argv[i];
+    }
+  }
+
   CZ80 z80;
 
-  if (argc > 1)
-    z80.load(argv[1]);
+  if (filename != "") {
+    if (binary)
+      z80.loadBin(filename);
+    else
+      z80.load(filename);
+  }
 
   CQZ80Dbg dbg(&z80);
 
